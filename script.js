2505e4f16735d96a37323c8dde57bd219dc904fa
Elmira811 <elmira.zyl92@gmail.com>
2025-01-07 13:56:46 +0300

    Update script.js
    Написала script для задания Forecast generator

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const button = document.querySelector('.forecast-btn');
+const currentForecast = document.querySelector('.current-forecast h1');
+const percent = document.querySelector('.current-forecast p');
+const forecastItem = document.querySelector('#forecast-item');
+const container = document.querySelector('.forecasts');
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+function makeMyPrediction(prediction, probability) {
+  const myPrediction = forecastItem.content.cloneNode(true);
+
+  myPrediction.querySelector('.forecast-item h3').textContent = prediction;
+  myPrediction.querySelector('.forecast-item p').textContent = probability;
+
+  return myPrediction;
+}
+
+button.addEventListener('click', function() {
+  let predictionNumber = getRandomInt(1, 5);
+  let predictionText = "";
+
+  switch(predictionNumber) {
+    case 1:
+      predictionText = "Сегодня произойдет приятное знакомство";
+      break;
+    case 2:
+      predictionText = "В этом месяце тебя ждет дальняя дорога";
+      break;
+    case 3:
+      predictionText = "Найдешь то, что потеряла";
+      break;
+    case 4:
+      predictionText = "Тебя ждет приятный сюрприз от близких";
+      break;
+    case 5:
+      predictionText = "Скоро исполнится заветное желание";
+      break;
+  }
+  
+  currentForecast.textContent = predictionText;
+
+  percent.textContent = `Вероятность: ${getRandomInt(0, 100)}%`;
+
+  const forecastCard = makeMyPrediction(currentForecast.textContent, percent.textContent);
+  
+  button.addEventListener('click', function() {
+    container.prepend(forecastCard);
+  })
+})
+
+
+
+
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
